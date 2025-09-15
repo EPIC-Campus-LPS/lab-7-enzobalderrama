5c286c634e669b0a78863f801e15a8c8a0ab3e50
enzobalderrama <enzobalderrama123@gmail.com>
2025-09-15 11:09:31 -0600

    Missed part of Lab 7
    My bad, will finish soon

---
+++ src/packageName/emailsIForgot.java
@@ -0,0 +1,12 @@
+package packageName;
+import java.util.Scanner;
+
+public class emailsIForgot {
+	Scanner scan = new Scanner(System.in);
+	String email = scan.nextLine();
+	int bad = email.indexOf("@");
+	String randomString = "";
+	for (int i = 0; i < email.length(); i++) {
+		
+	}
+}
